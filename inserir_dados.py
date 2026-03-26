d26d7be9324a35ad245851751bd4af099be04571
JoaoBrelazsz <joao.brelaz@uni9.edu.br>
2026-03-25 23:09:22 -0300

    Update inserir_dados.py

--- inserir_dados.py
+++ inserir_dados.py
@@ -1,19 +1,24 @@
 import psycopg2
 import os
 import random
+from datetime import datetime
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 conn = psycopg2.connect(DATABASE_URL)
 cursor = conn.cursor()
 
-nome = f"Cliente_{random.randint(1,1000)}"
+for i in range(50):
+    nome = f"Cliente_{random.randint(1,10000)}"
+    estado = random.choice(["SP", "RJ", "MG", "BA"])
+    cidade = "Cidade_" + str(random.randint(1,100))
+    score = round(random.uniform(5,10),1)
 
-cursor.execute("""
-INSERT INTO "CLIENTES" (nome, estado, cidade, score, data_cadastro)
-VALUES (%s, %s, %s, %s, CURRENT_DATE)
-""", (nome, "SP", "São Paulo", 8.0))
+    cursor.execute("""
+    INSERT INTO "CLIENTES" (nome, estado, cidade, score, data_cadastro)
+    VALUES (%s, %s, %s, %s, CURRENT_DATE)
+    """, (nome, estado, cidade, score))
 
 conn.commit()
 
-print("Inserido!")
+print("50 clientes inseridos!", datetime.now())
